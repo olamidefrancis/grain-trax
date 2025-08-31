84e390ccfebf7a14a96ce7c80965aa369d93bb8e
deji2612 <olatigbay@gmail.com>
2025-08-31 17:58:02 +0100

    Industry section fix

--- src/assets/Component/Clouds/Clouds.jsx
+++ src/assets/Component/Clouds/Clouds.jsx
@@ -5,7 +5,12 @@ import './Clouds.css'
 const Clouds=()=> {
     return (
         <div className="cloud__container">
-                <p>hello</p>
+            <div className="bordercon">
+                      <h2 className="company__name--title">
+                            <span style={{color: "red"}}>Grain</span>&Trax
+                      </h2>
+            </div>
+              
         </div>
     )
 }
--- src/assets/Component/Industries/Industries.jsx
+++ src/assets/Component/Industries/Industries.jsx
@@ -1,90 +1,144 @@
 import './Industries.css'
-import target from "../../Images/icons8-target-color-hand-drawn-70.png"
+// import target from "../../Images/icons8-target-color-hand-drawn-70.png"
+import map from "../../Images/icons8-map-96.png"
 
 const Industries=()=> {
     return (
-        <div className="industries">
-               <h1 className="industries__title">
-                 Industries
-               </h1>
-               <h2 className='industries__intro'>Build Strategies, Build Confidence, Build Your Business.</h2> 
+        // <div className="industries">
+        //        <h1 className="industries__title">
+        //          Industries
+        //        </h1>
+        //        <h2 className='industries__intro'>Build Strategies, Build Confidence, Build Your Business.</h2> 
 
-               <div className="industries__grid-container">
-                    <div className="industries__grid-container1">
-                        <figure>
-                            <img src={target} alt="target" />
-                        </figure>
+        //        <div className="industries__grid-container">
+        //             <div className="industries__grid-container1">
+        //                 <figure>
+        //                     <img src={target} alt="target" />
+        //                 </figure>
 
-                        <div className="industries__grid-container1--content">
-                            <h2>Life Sciences & Health Care</h2>
-                            <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum explicabo libero velit culpa!</h3>
-                        </div>
+        //                 <div className="industries__grid-container1--content">
+        //                     <h2>Life Sciences & Health Care</h2>
+        //                     <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum explicabo libero velit culpa!</h3>
+        //                 </div>
 
-                    </div>
-                    <div className="industries__grid-container2">
-                         <figure>
-                            <img src={target} alt="target" />
-                        </figure>
+        //             </div>
+        //             <div className="industries__grid-container2">
+        //                  <figure>
+        //                     <img src={target} alt="target" />
+        //                 </figure>
 
-                        <div className="industries__grid-container1--content">
-                            <h2>Life Sciences & Health Care</h2>
-                            <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum explicabo libero velit culpa!</h3>
-                        </div>
-                    </div>
-                    <div className="industries__grid-container3">
-                        <figure>
-                            <img src={target} alt="target" />
-                        </figure>
+        //                 <div className="industries__grid-container1--content">
+        //                     <h2>Life Sciences & Health Care</h2>
+        //                     <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum explicabo libero velit culpa!</h3>
+        //                 </div>
+        //             </div>
+        //             <div className="industries__grid-container3">
+        //                 <figure>
+        //                     <img src={target} alt="target" />
+        //                 </figure>
 
-                        <div className="industries__grid-container1--content">
-                            <h2>Life Sciences & Health Care</h2>
-                            <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum explicabo libero velit culpa!</h3>
-                        </div>
-                    </div>
-                    <div className="industries__grid-container4">
-                        <figure>
-                            <img src={target} alt="target" />
-                        </figure>
+        //                 <div className="industries__grid-container1--content">
+        //                     <h2>Life Sciences & Health Care</h2>
+        //                     <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum explicabo libero velit culpa!</h3>
+        //                 </div>
+        //             </div>
+        //             <div className="industries__grid-container4">
+        //                 <figure>
+        //                     <img src={target} alt="target" />
+        //                 </figure>
 
-                        <div className="industries__grid-container1--content">
-                            <h2>Life Sciences & Health Care</h2>
-                            <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum explicabo libero velit culpa!</h3>
-                        </div>
-                    </div>
-                    <div className="industries__grid-container4">
-                        <figure>
-                            <img src={target} alt="target" />
-                        </figure>
+        //                 <div className="industries__grid-container1--content">
+        //                     <h2>Life Sciences & Health Care</h2>
+        //                     <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum explicabo libero velit culpa!</h3>
+        //                 </div>
+        //             </div>
+        //             <div className="industries__grid-container4">
+        //                 <figure>
+        //                     <img src={target} alt="target" />
+        //                 </figure>
 
-                        <div className="industries__grid-container1--content">
-                            <h2>Life Sciences & Health Care</h2>
-                            <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum explicabo libero velit culpa!</h3>
-                        </div>
-                    </div>
-                    <div className="industries__grid-container4">
-                        <figure>
-                            <img src={target} alt="target" />
-                        </figure>
+        //                 <div className="industries__grid-container1--content">
+        //                     <h2>Life Sciences & Health Care</h2>
+        //                     <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum explicabo libero velit culpa!</h3>
+        //                 </div>
+        //             </div>
+        //             <div className="industries__grid-container4">
+        //                 <figure>
+        //                     <img src={target} alt="target" />
+        //                 </figure>
 
-                        <div className="industries__grid-container1--content">
-                            <h2>Life Sciences & Health Care</h2>
-                            <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum explicabo libero velit culpa!</h3>
-                        </div>
-                    </div>
-                    <div className="industries__grid-container4">
-                        <figure>
-                            <img src={target} alt="target" />
-                        </figure>
+        //                 <div className="industries__grid-container1--content">
+        //                     <h2>Life Sciences & Health Care</h2>
+        //                     <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum explicabo libero velit culpa!</h3>
+        //                 </div>
+        //             </div>
+        //             <div className="industries__grid-container4">
+        //                 <figure>
+        //                     <img src={target} alt="target" />
+        //                 </figure>
 
-                        <div className="industries__grid-container1--content">
-                            <h2>Life Sciences & Health Care</h2>
-                            <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum explicabo libero velit culpa!</h3>
-                        </div>
-                    </div>
+        //                 <div className="industries__grid-container1--content">
+        //                     <h2>Life Sciences & Health Care</h2>
+        //                     <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum explicabo libero velit culpa!</h3>
+        //                 </div>
+        //             </div>
                     
 
-               </div>
+        //        </div>
 
+        // </div>
+
+        <div className="industry__con">
+            
+            <div className="industry__con--content">
+                <div className="industry__con--content--num">
+                    <h1>8</h1>
+                    <p>Lorem ipsum dolor sit amet repudiandae eligendi.</p>
+                    <p>consectetur, adipisicing elit. Quibusdam rerum nobis</p>
+                </div>
+                
+                <div className="industry__con--content--num--iages">
+                    <img src={map} alt="mapme" />
+                </div>
+            </div>
+
+            <div className="industry__con--content">
+                <div className="industry__con--content--num">
+                    <h1>8</h1>
+                    <p>Lorem ipsum dolor sit amet repudiandae eligendi.</p>
+                    <p>consectetur, adipisicing elit. Quibusdam rerum nobis</p>
+                </div>
+                
+                <div className="industry__con--content--num--iages">
+                    <img src={map} alt="mapme" />
+                </div>
+            </div>
+
+            <div className="industry__con--content">
+                <div className="industry__con--content--num">
+                    <h1>8</h1>
+                    <p>Lorem ipsum dolor sit amet repudiandae eligendi.</p>
+                    <p>consectetur, adipisicing elit. Quibusdam rerum nobis</p>
+                </div>
+                
+                <div className="industry__con--content--num--iages">
+                    <img src={map} alt="mapme" />
+                </div>
+            </div>
+
+            <div className="industry__con--content">
+                <div className="industry__con--content--num">
+                    <h1>8</h1>
+                    <p>Lorem ipsum dolor sit amet repudiandae eligendi.</p>
+                    <p>consectetur, adipisicing elit. Quibusdam rerum nobis</p>
+                </div>
+                
+                <div className="industry__con--content--num--iages">
+                    <img src={map} alt="mapme" />
+                </div>
+            </div>
+
+            
         </div>
     )
 }
