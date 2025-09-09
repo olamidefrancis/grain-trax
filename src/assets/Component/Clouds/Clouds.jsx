41d71906e0e8931674e322a1183d7c6d28b12eaf
deji2612 <olatigbay@gmail.com>
2025-09-09 12:01:27 +0100

    About us content

--- src/assets/Component/Clouds/Clouds.jsx
+++ src/assets/Component/Clouds/Clouds.jsx
@@ -14,8 +14,21 @@ const Clouds=()=> {
                 <h2 className="cloud__container-aboutus-aboutus">
                     About Us
                 </h2>
-                <div className="cloud__container-aboutus-content">
-                    
+                <div className="cloud__container-aboutus-content" style={{lineHeight:"180%"}}>
+                    <h3>Reliable procurement of Healthcare equipment | Certified Medical Devices & Supplies, Agricultural equipment | Tractors, and Precious Metals | Gold, Silver & Platinum</h3>
+
+                    <div className="cloud__container-aboutus-content-whoweserve">
+                        <h4>
+                            Who we serve
+                        </h4>
+
+                        <ul className="cloud__container-aboutus-content-whoweserve-arrow" style={{lineHeight:"180%"}}>
+                                <li>Financial institutions</li>
+                                <li>Jewellers and refiners</li>
+                                <li>Industrial manufacturers</li>
+                                <li>Private investors and collectors</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
               
