e9301b886f59e746f7f5cc34ea441b4fd68dae0f
deji2612 <olatigbay@gmail.com>
2025-09-09 09:37:26 +0100

    modification

--- src/assets/Component/Clouds/Clouds.jsx
+++ src/assets/Component/Clouds/Clouds.jsx
@@ -5,10 +5,18 @@ import './Clouds.css'
 const Clouds=()=> {
     return (
         <div className="cloud__container">
-            <div className="bordercon">
+            {/* <div className="bordercon">
                       <h2 className="company__name--title">
                             <span style={{color: "red"}}>Grain</span>&Trax
                       </h2>
+            </div> */}
+            <div className="cloud__container-aboutus">
+                <h2 className="cloud__container-aboutus-aboutus">
+                    About Us
+                </h2>
+                <div className="cloud__container-aboutus-content">
+                    
+                </div>
             </div>
               
         </div>
